0716c3f5a88787f82c485ad1a53239fda1883f6c
Your Name <you@example.com>
2020-05-17 18:18:38 +1000

    change layout pages

--- script-friend.js
+++ script-friend.js
@@ -15,11 +15,8 @@ const Winning_Combinations = [
     [0, 4, 8],
     [2, 4, 6]
 ]
-let possible_win_combo = [];
-Winning_Combinations.forEach(c =>
-    possible_win_combo.push(combinations(c).filter(a => a.length == 2)));
-const number = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let circleTurn;
+
 
 function startGame() {
     circleTurn = false;
@@ -49,7 +46,7 @@ function handleClick(e) {
         swapTurns();
         setBoardHoverClass();
         if (circleTurn) {
-            setTimeout(placeCircleMark(currentClass), 700)
+            setInterval(placeCircleMark, 2000)
         }
     }
 
@@ -58,7 +55,7 @@ function isGameDraw(draw) {
     if (draw) {
         Winning_Message_Text.innerHTML = 'Draw!'
     } else {
-        Winning_Message_Text.innerHTML = `${circleTurn ? "O's" : "You"} Win!`
+        Winning_Message_Text.innerHTML = `${circleTurn ? "O's" : "X's"} Wins`
     }
     Winning_Message.classList.add('show');
 }
@@ -95,54 +92,11 @@ const checkWin = (currentClass) => {
     })
 }
 
-const placeCircleMark = (currentClass) => {
-    let xIndexs = findXIndex();
-    let circleIndexs = findCircleIndex();
-    let numberTemp = [...number];
-    numberTemp = numberTemp.filter(e => ![...xIndexs, ...circleIndexs].includes(e))
-    let random = numberTemp[Math.floor(Math.random() * (numberTemp.length - 1))];
-    //circle strategy
-    if (numberTemp.includes(4)) {
-        random = 4;
-    }
-    else if (xGoingToWin(xIndexs)) {
-        //random ==winningpossibleIndex
-        /* const possible = Winning_Combinations.find(item => item.filter(i => !xIndexs.includes(i)));
-        const index = possible.filter(item => !xIndexs.includes(item));
-        if (!numberTemp.includes(index)) {
-            random = parseInt(index.toString());
-            Winning_Combinations.splice(Winning_Combinations.indexOf(possible), 1); */
-    }
+const placeCircleMark = () => {
+    cellElements.filter(cell => {
+        return cell.classList != x_class || cell.classList != circle_class;
+    })
+}
 
-    numberTemp.splice(numberTemp.indexOf(random), 1)
-    cellElements[random].classList.add(circle_class);
-    console.log("number", numberTemp, "random", random)
-    if (checkWin(circle_class)) {
-        isGameDraw(false);
-    } else if (isDraw()) {
-        isGameDraw(true);
-    }
-    swapTurns();
-    setBoardHoverClass();
-
-
-}
-function findXIndex() {
-    return [...cellElements].map((cell, index) =>
-        cell.classList.contains(x_class) ? index : '').filter(String)
-}
-function findCircleIndex() {
-    return [...cellElements].map((cell, index) =>
-        cell.classList.contains(circle_class) ? index : '').filter(String)
-}
-function xGoingToWin(xIndexs) {
-    //check if x insists in possible combo
-    return possible_win_combo.some(combo =>
-        combo.some(c => c.every(x => xIndexs.includes(x))))
-}
-function combinations(array) {
-    return new Array(1 << array.length).fill().map(
-        (e1, i) => array.filter((e2, j) => i & 1 << j));
-}
 startGame();
 restart.addEventListener('click', startGame);
